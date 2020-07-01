a0fb338752dd669e7e39b1a3399693fba9c3f1de
Mike Stubna <mike@stubna.com>
2020-06-30 21:47:34 -0400

    update images

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+import gear1 from './pin1.svg'
+import gear2 from './pin2.svg'
+import pin1 from './pin1.png'
+import pin2 from './pin2.png'
+import pin3 from './pin3.png'
+import pin4 from './pin4.png'
+
+export { gear1, gear2, pin1, pin2, pin3, pin4 }
